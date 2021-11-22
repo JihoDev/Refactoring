3f6f1aa1cdbe4f396af5302b5ee6942b871914db
Jiho Kim <jiho@kakao.com>
2021-11-22 18:36:20 +0900

    refactor: rename variable - amountFor

--- Martin Fowler/Javascript/index.js
+++ Martin Fowler/Javascript/index.js
@@ -29,24 +29,24 @@ function statement(invoice, plays) {
 
 // 값이 바뀌지 않는 변수는 매개변수로 전달
 function amountFor(perf, play) {
-  let thisAmount = 0; // 변수를 초기화하는 코드
+  let result = 0; // 변수를 초기화하는 코드
   switch (play.type) {
     case "tragedy": // 비극
-      thisAmount = 40000;
+      result = 40000;
       if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+        result += 1000 * (perf.audience - 30);
       }
       break;
     case "comedy": // 희극
-      thisAmount = 30000;
+      result = 30000;
       if (perf.audience > 20) {
-        thisAmount += 1000 * (perf.audience - 20);
+        result += 1000 * (perf.audience - 20);
       }
       break;
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
   }
-  return thisAmount; // 함수 안에서 값이 바뀌는 변수 반환
+  return result; // 함수 안에서 값이 바뀌는 변수 반환
 }
 
 function main() {
