da0452a70e140fb04d682a0b2c640f49fb97f9bb
Jiho Kim <jiho@kakao.com>
2021-11-23 12:25:05 +0900

    refactor: chage function declaration - format to usd

--- Martin Fowler/Javascript/index.js
+++ Martin Fowler/Javascript/index.js
@@ -10,10 +10,10 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf); // 추출한 함수를 이용해 값을 누적
 
     // 청구 내역을 출력한다.
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} 석)\n`; // 변수 인라인
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} 석)\n`; // 변수 인라인
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${format(totalAmount/100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits} 점\n`;
   return result;
 }
@@ -53,10 +53,10 @@ function volumeCreditsFor(perf) {
   return result;
 }
 
-function format(aNumber) {
+function usd(aNumber) {
   return new Intl.NumberFormat("en-US",
                       { style: "currency", currency: "USD",
-                        minimumFractionDigits: 2 }).format(aNumber);
+                        minimumFractionDigits: 2 }).format(aNumber/100);
 }
 
 function main() {
