ff24666d9ecb98f2f552149bac23307b8fdc7c88
Jiho Kim <jiho@kakao.com>
2021-11-23 12:15:53 +0900

    refactor: inline variable - thisAmount

--- Martin Fowler/Javascript/index.js
+++ Martin Fowler/Javascript/index.js
@@ -10,16 +10,14 @@ function statement(invoice, plays) {
                           minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf); // 필요 없어진 매개변수 제거
-
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
     if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5); // 변수 인라인
 
     // 청구 내역을 출력한다.
-    result += `  ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} 석)\n`; // 변수 인라인
-    totalAmount += thisAmount;
+    result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} 석)\n`; // 변수 인라인
+    totalAmount += amountFor(perf);
   }
   result += `총액: ${format(totalAmount/100)}\n`;
   result += `적립 포인트: ${volumeCredits} 점\n`;
