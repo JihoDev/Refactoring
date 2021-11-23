f084c04c3cf417b7c4c71aec9fce63d029a0d4f6
Jiho Kim <jiho@kakao.com>
2021-11-23 12:23:10 +0900

    refactor: chage variable to function - format

--- Martin Fowler/Javascript/index.js
+++ Martin Fowler/Javascript/index.js
@@ -5,9 +5,6 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US",
-                        { style: "currency", currency: "USD",
-                          minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf); // 추출한 함수를 이용해 값을 누적
@@ -54,6 +51,12 @@ function volumeCreditsFor(perf) {
   if ("comedy" === playFor(perf).type)
     result += Math.floor(perf.audience / 5);
   return result;
+}
+
+function format(aNumber) {
+  return new Intl.NumberFormat("en-US",
+                      { style: "currency", currency: "USD",
+                        minimumFractionDigits: 2 }).format(aNumber);
 }
 
 function main() {
